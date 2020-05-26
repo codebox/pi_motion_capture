db04e47381e87fd44df4407b82fc022d2e867a80
rob <rob@codebox.org.uk>
2020-05-26 09:31:16 +0100

    setup script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+ENV_DIR=env
+
+function activate {
+    source ${ENV_DIR}/bin/activate
+}
+
+if [ ! -d $ENV_DIR ]; then
+    python3 -m venv $ENV_DIR
+    activate
+    pip install opencv-python
+else
+    activate
+fi
